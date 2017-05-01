5ae324ea669e254c1a65bceadacb154bae078f41
Bryan Burnham <thedopfien65@gmail.com>
2017-05-01 05:39:17 +0000

    Not what I wanted, but as good as it's gonna get at this point

--- comment/models.py
+++ comment/models.py
@@ -5,3 +5,12 @@ class Comment(models.Model):
     comment_author = models.CharField(max_length = 40)
     comment_date = models.DateTimeField('date published')
     comment_rating = models.IntegerField()
+    
+    def _str_(self):
+        return self.comment_text
+        
+    def upvote(self):
+        self.comment_rating += 1
+    
+    def downvote(self):
+        self.comment_rating -= 1
--- comment/views.py
+++ comment/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from models import Comment
 
-class IndexView(generic.TemplateView):
+class IndexView(generic.ListView):
     template_name = 'comment/index.html'
     context_object_name = 'comment_board'
+    def get_queryset(self):
+        return Comment.objects.order_by('-comment_rating')
